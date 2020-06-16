06b562a869dbe294977e18263d5c0df7a5efdb7a
Rhea Anastasia <rheajoulin@gmail.com>
2020-06-16 21:52:49 +0200

    Login branch added

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,16 +2,11 @@
 import React from 'react';
 
 // == Import
-import Counter from 'src/containers/Counter';
-import reactLogo from './react-logo.svg';
-import './styles.css';
+import './style.scss';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
-    <Counter />
   </div>
 );
 
