28af63184ef38a6c56005b8adbc2971d9277db4f
Rhea Anastasia <rheajoulin@gmail.com>
2020-06-26 12:42:39 +0200

    css changed for prevent bugs

--- src/components/Room/SharedView/index.js
+++ src/components/Room/SharedView/index.js
@@ -3,10 +3,11 @@ import React from 'react';
 import './style.scss';
 
 const SharedView = () => (
-  <div className="b">
+  <div className="shared-view">
     <img
-      src="https://www.hp-lexicon.org/wp-content/uploads/2016/05/gryffindor-dorms.jpg"
+      src="https://i1.wp.com/gab.ae/wp-content/uploads/2018/02/ECx.jpg?w=1920&ssl=1"
       alt="shared screen"
+      className="shared-view-img"
     />
   </div>
 );
