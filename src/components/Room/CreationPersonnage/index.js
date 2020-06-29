f1d4229604f0dbb3bfb588e77d28f97349709d79
Mehdicament <delandemare.mehdi@gmail.com>
2020-06-29 16:02:29 +0200

    Modification du form creation de personnage

--- src/components/Room/CreationPersonnage/index.js
+++ src/components/Room/CreationPersonnage/index.js
@@ -4,100 +4,80 @@ import PropTypes from 'prop-types';
 import Field from '../Room/Forms/Field';
 
 const CreationPersonnage = ({
-  nom,
-  maison,
-  age,
-  statut,
-  animal,
+  Name,
+  Age,
+  Animal,
+  Maison,
+  Statut,
   changeValue,
+
 }) => {
   const handleSubmit = (evt) => {
     evt.preventDefault();
-    console.log('je veux envoyer les nouvelles infos du profil au serveur');
-    sendEditedDatas();
   };
 
   return (
-    <div className="creation-personnage">
-      <div className="creation-personnage-title">
-        <h1>Création de ta fiche de personnage !</h1>
-      </div>
-      <div className="creation-personnage-form">
-        <Form onSubmit={handleSubmit}>
-          <Form.Field>
-            <label htmlFor="nom" className="creation-personnage-form-label">Nom Prénom de ton personnage
-              <Field
-                className="creation-personnage-form-input"
-                type="text"
-                placeholder="nom"
-                name="nom"
-                changeValue={changeValue}
-                value={nom}
-              />
-            </label>
-          </Form.Field>
-          <Form.Field>
-            <label control="select" className="creation-personnage-form-label">Maison du personnage ?
-              <Field
-                type="text"
-                placeholder="maison"
-                name="maison"
-                changeValue={changeValue}
-                value={maison}
-              />
-              <option value="gryffindor">Gryffindor</option>
-              <option value="poufsouffle">Poufsouffle</option>
-              <option value="serdaigie">Serdaigie</option>
-              <option value="serpentard">Serpentard</option>
-            </label>
-          </Form.Field>
-          <Form.Field>
-            <label htmlFor="age" className="creation-personnage-form-label">Son âge ?
-              <Field
-                type="text"
-                placeholder="age"
-                name="age"
-                changeValue={changeValue}
-                value={age}
-              />
-            </label>
-          </Form.Field>
-          <Form.Field>
-            <label htmlFor="statut" className="creation-personnage-form-label">Son statut ?
-              <Field
-                type="text"
-                placeholder="statut"
-                name="statut"
-                changeValue={changeValue}
-                value={statut}
-              />
-            </label>
-          </Form.Field>
-          <Form.Field>
-            <label htmlFor="animal" className="creation-personnage-form-label">Son animal de compagnie ?
-              <Field
-                type="text"
-                placeholder="animals"
-                name="animal"
-                changeValue={changeValue}
-                value={animal}
-              />
-            </label>
-          </Form.Field>
-          <Button type="submit">Tues prêt? c'est parti !</Button>
-        </Form>
-      </div>
-    </div>
+    <Form onSubmit={handleSubmit}>
+      <Form.Field label="Nom Prénom de ton personnage" control="input">
+        <Field
+          type="text"
+          placeholder="Nom Prénom de ton personnage"
+          name="Name"
+          changeValue={changeValue}
+          value={Name}
+        />
+      </Form.Field>
+      <Form.Field label="Choisi la maison de ton personnage" control="select">
+        <option value="griffondor">Griffondor</option>
+        <option value="poufsouffle">Poufsouffle</option>
+        <Field
+          type="option"
+          placeholder="Choisi la maison de ton persnnage"
+          name="Maison"
+          changeValue={changeValue}
+          value={Maison}
+        />
+      </Form.Field>
+      <Form.Field label="L'âge de ton personnage ?" control="input">
+        <Field
+          type="text"
+          placeholder="L'âge de ton personnage ?"
+          name="Age"
+          changeValue={changeValue}
+          value={Age}
+        />
+      </Form.Field>
+      <Form.Field label="Le statut de ton personnage ?" control="input">
+        <Field
+          type="text"
+          placeholder="Le statut de ton personnage ?"
+          name="Statut"
+          changeValue={changeValue}
+          value={Statut}
+        />
+      </Form.Field>
+      <Form.Field label="L'animal de compagnie de ton personnage ?" control="input">
+        <Field
+          type="text"
+          placeholder="L'animal de compagnie de ton personnge ?"
+          name="Animal"
+          changeValue={changeValue}
+          value={Animal}
+        />
+      </Form.Field>
+      <Button color="black" type="submit" className="center aligned">
+        Tu es prêt ? C'est parti !
+      </Button>
+    </Form>
   );
 };
-
 CreationPersonnage.propTypes = {
-  nom: PropTypes.string.isRequired,
-  maison: PropTypes.string.isRequired,
-  age: PropTypes.string.isRequired,
-  statut: PropTypes.string.isRequired,
-  animal: PropTypes.string.isRequired,
+  Name: PropTypes.string.isRequired,
+  Age: PropTypes.string.isRequired,
+  Maison: PropTypes.string.isRequired,
   changeValue: PropTypes.func.isRequired,
+  Animal: PropTypes.string.isRequired,
+  Statut: PropTypes.string.isRequired,
 };
 
 export default CreationPersonnage;
