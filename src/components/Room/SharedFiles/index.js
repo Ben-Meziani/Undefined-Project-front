47bcaf8f9c8c74e2d357cfff41eab0314301c859
severinebianchi <severine.bianchi78@gmail.com>
2020-06-29 11:47:09 +0200

    Test for upload request - update server

--- src/components/Room/SharedFiles/index.js
+++ src/components/Room/SharedFiles/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
 import PropTypes from 'prop-types';
 
@@ -5,9 +6,10 @@ import { Icon } from 'semantic-ui-react';
 
 import './style.scss';
 
-const SharedFiles = ({ bookOpen }) => {
+const SharedFiles = ({ toggleOpenBook, bookOpen }) => {
   return (
     <div className={!bookOpen ? 'shared-files-toggle shared-files' : 'shared-files '}>
+      <button type="button" onClick={toggleOpenBook}><Icon name="close" size="medium" /></button>
       <div><Icon name="book" size="huge" /></div>
       <div><Icon name="book" size="huge" /></div>
       <div><Icon name="book" size="huge" /></div>
@@ -26,6 +28,7 @@ const SharedFiles = ({ bookOpen }) => {
 
 SharedFiles.propTypes = {
   bookOpen: PropTypes.bool.isRequired,
+  toggleOpenBook: PropTypes.func.isRequired,
 };
 
 export default SharedFiles;
