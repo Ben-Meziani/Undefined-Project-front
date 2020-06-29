3cde2dec4ca6f29de30b5763efd62976daeaf97b
Rhea Anastasia <rheajoulin@gmail.com>
2020-06-29 23:02:54 +0200

    video test

---
+++ src/components/HomePage/Video/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactPlayer from 'react-player/youtube';
+
+const Video = () => (
+  // Render a YouTube video player
+  <ReactPlayer url="https://www.youtube.com/watch?v=pAHciSqi1-8" />
+
+);
+
+export default Video;
