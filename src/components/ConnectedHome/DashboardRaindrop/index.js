d4397fed71d1bbddd357924900e3b619d9389836
severinebianchi <severine.bianchi78@gmail.com>
2020-07-05 23:16:07 +0200

    update server

--- src/components/ConnectedHome/DashboardRaindrop/index.js
+++ src/components/ConnectedHome/DashboardRaindrop/index.js
@@ -6,30 +6,32 @@ import raindrop from 'src/assets/white-raindrop.png';
 
 import './style.scss';
 
+const imageUrl = `https://undefined-project.tk/${raindrop}`;
+
 const DashboardRainDrop = () => {
   console.log('raindrop vaut', raindrop);
   return (
     <div className="dashboard-raindrop-animation">
       <div className="white-raindrop1">
-        <img src={raindrop} alt="white raindrop" />
+        <img src={imageUrl} alt="white raindrop" />
       </div>
       <div className="white-raindrop2">
-        <img src={raindrop} alt="white raindrop" />
+        <img src={imageUrl} alt="white raindrop" />
       </div>
       <div className="white-raindrop3">
-        <img src={raindrop} alt="white raindrop" />
+        <img src={imageUrl} alt="white raindrop" />
       </div>
       <div className="white-raindrop4">
-        <img src={raindrop} alt="white raindrop" />
+        <img src={imageUrl} alt="white raindrop" />
       </div>
       <div className="white-raindrop5">
-        <img src={raindrop} alt="white raindrop" />
+        <img src={imageUrl} alt="white raindrop" />
       </div>
       <div className="white-raindrop6">
-        <img src={raindrop} alt="white raindrop" />
+        <img src={imageUrl} alt="white raindrop" />
       </div>
       <div className="white-raindrop7">
-        <img src={raindrop} alt="white raindrop" />
+        <img src={imageUrl} alt="white raindrop" />
       </div>
      {/*  <div className="white-raindrop8">
         <img src={raindrop} alt="white raindrop" />
