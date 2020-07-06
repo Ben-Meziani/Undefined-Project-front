a93f5ccceb5e37a91e8b48d0da31b6ce73f75aab
severinebianchi <severine.bianchi78@gmail.com>
2020-07-06 13:39:17 +0200

    update server

--- src/components/Room/PlayersList/Player/index.js
+++ src/components/Room/PlayersList/Player/index.js
@@ -7,20 +7,20 @@ import magician from '../../../../assets/icons/magician.png';
 
 const magicianUrl = `https://undefined-project.tk/${magician}`;
 
-const Player = ({ pseudo, icon }) => (
+const Player = ({ pseudo/* , icon */ }) => (
   <div className="player">
     <div className="player-pseudo">
       <p>{ pseudo }</p>
     </div>
     <div className="player-icon">
-      <img src={icon} alt="avatar" />
+      <img src={magicianUrl} alt="avatar" />
     </div>
   </div>
 );
 
 Player.propTypes = {
   pseudo: PropTypes.string.isRequired,
-  icon: PropTypes.string.isRequired,
+ /*  icon: PropTypes.string.isRequired, */
 };
 
 export default Player;
